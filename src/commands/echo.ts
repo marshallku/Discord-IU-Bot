e73274109fd974681359c606e186dc65d358feac
marshallku <marshall@kakao.com>
2022-03-25 22:22:13 +0900

    Remove useless code

--- src/commands/echo.ts
+++ src/commands/echo.ts
@@ -16,15 +16,7 @@ export default function echoToChannel({
     }
 
     if (content[content.length - 1] === "지워") {
-        channel.send(content.slice(0, -1).join(" ")).then(() => {
-            try {
-                msg.delete();
-            } catch (err) {
-                channel.send(
-                    "메시지 삭제 권한을 부여받지 못했습니다. 서버 관리자에게 문의해주세요.\nhttps://discordapp.com/api/oauth2/authorize?client_id=684667274287906835&permissions=8&scope=bot\n링크를 통해 봇을 추가하시면 문제가 해결됩니다."
-                );
-            }
-        });
+        channel.send(content.slice(0, -1).join(" "));
     } else {
         channel.send(content.join(" "));
     }
